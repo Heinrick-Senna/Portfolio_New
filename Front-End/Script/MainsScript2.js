52cc1db3a6fcc1072cf6598f826a3018ddda3d18
Marcelo Heinrick <marcelohz198@hotmail.com>
2020-07-31 18:38:21 -0300

    BetaPhase 0.0.6

--- Front-End/Script/MainsScript2.js
+++ Front-End/Script/MainsScript2.js
@@ -1,11 +1,17 @@
 window.onload = function () {
-	$('#FloatTheme li').click(function Theme (){
-		var actualTheme = 0 + actualTheme;
-		ThemeSwitch(clickTheme = $(this).attr('id').replace(/[^0-9]/g,''), verifyTheme = actualTheme);
+	$('#FloatTheme li').click(function(){
+		clickTheme=$(this).attr('id').replace(/[^0-9]/g,'');
+		Theme(clickTheme);
 	})
 }
 
-function ThemeSwitch () {
+function Theme (){
+		var actualTheme = 0 + parseInt(actualTheme, 10);
+		alert(actualTheme);
+		ThemeSwitch(clickTheme, verifyTheme = actualTheme);
+	}
+
+function ThemeSwitch (verifyTheme=actualTheme) {
 	if (clickTheme != verifyTheme) {
 		var themesIco = document.getElementById('FloatTheme').getElementsByTagName('li');
 		if (clickTheme == 0){
@@ -16,6 +22,7 @@ function ThemeSwitch () {
 			themesIco[clickTheme].innerHTML = '<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-dice-2-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M0 3a3 3 0 0 1 3-3h10a3 3 0 0 1 3 3v10a3 3 0 0 1-3 3H3a3 3 0 0 1-3-3V3zm5.5 1a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm6.5 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z">';
 			themesIco[0].innerHTML = '<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-dice-1" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M13 1H3a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2zM3 0a3 3 0 0 0-3 3v10a3 3 0 0 0 3 3h10a3 3 0 0 0 3-3V3a3 3 0 0 0-3-3H3z"/><circle cx="8" cy="8" r="1.5"/>';
 			themesIco[2].innerHTML = '<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-dice-3" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M13 1H3a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2zM3 0a3 3 0 0 0-3 3v10a3 3 0 0 0 3 3h10a3 3 0 0 0 3-3V3a3 3 0 0 0-3-3H3z"/><circle cx="4" cy="4" r="1.5"/> <circle cx="12" cy="12" r="1.5"/> <circle cx="8" cy="8" r="1.5"/>';
+			alert('a');
 		}
 		if (clickTheme == 2){
 			themesIco[clickTheme].innerHTML = '<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-dice-3-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 0a3 3 0 0 0-3 3v10a3 3 0 0 0 3 3h10a3 3 0 0 0 3-3V3a3 3 0 0 0-3-3H3zm2.5 4a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm8 8a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zM8 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z"/>';
