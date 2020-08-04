e9336bac69eacc6378db837bdd77e2ed3854c0e8
Marcelo Heinrick <marcelohz198@hotmail.com>
2020-08-04 01:25:42 -0300

    BetaPhase 0.0.8

--- Front-End/Script/MainsScript2.js
+++ Front-End/Script/MainsScript2.js
@@ -7,12 +7,7 @@ window.onload = function () {
 function Animation () {
 	var FirstLetter = document.getElementById('FirstHalf');
 	var SecondLetter = document.getElementById('SecondHalf');
-	var ConnectorLetter = document.getElementById('ConnectorLetter');
-	FirstLetter.setAttribute('style', 'position:absolute;margin-top:-10%;');
-	SecondLetter.setAttribute('style', 'position:absolute;margin-top:10%;');	
-	ConnectorLetter.setAttribute('style', 'position:absolute;margin-top:10%');
-	setTimeout(function(){
-		ConnectorLetter.setAttribute('src', '');
-		SecondLetter.setAttribute('src', '../Design/HalfLogo2.png');
-	}, 1000);
+	var divcontainer = document.getElementsByTagName('div')[0];
+	var divcontainer2 = document.getElementsByTagName('div')[1];
+	SecondLetter.setAttribute('style', 'margin-top:200%');
 }
