3a9ce7aada8fc122094cda77c74561535ac36e7e
Marcelo Heinrick <marcelohz198@hotmail.com>
2020-08-09 00:41:00 -0300

    BetaPhase 0.1.0

--- Front-End/Script/MainScript1.js
+++ Front-End/Script/MainScript1.js
@@ -1,4 +1,8 @@
 $(document).ready(function(){
+	setTimeout(function(){
+	}, 1000);
+
+
 	$('#FloatTheme li').click(function (){
 		var actualTheme = parseInt($('#ThemeLink').attr('href').substr(-5).replace(/[^0-9]/g,''));
 		var clickTheme=$(this).attr('id').replace(/[^0-9]/g,'');
@@ -30,7 +34,7 @@ function ThemeSwitch (clickTheme) {
 			themesIco[0].innerHTML = '<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-dice-1" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M13 1H3a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2zM3 0a3 3 0 0 0-3 3v10a3 3 0 0 0 3 3h10a3 3 0 0 0 3-3V3a3 3 0 0 0-3-3H3z"/><circle cx="8" cy="8" r="1.5"/>';
 			themesIco[1].innerHTML = '<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-dice-2" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M13 1H3a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2zM3 0a3 3 0 0 0-3 3v10a3 3 0 0 0 3 3h10a3 3 0 0 0 3-3V3a3 3 0 0 0-3-3H3z"/><circle cx="4" cy="4" r="1.5"/><circle cx="12" cy="12" r="1.5"/>';
 		}
-		var newTheme = 'CSS/Design' + clickTheme + '.css';
+		var newTheme = 'CSS/Colors' + clickTheme + '.css';
 		document.getElementById('ThemeLink').setAttribute('href', newTheme);
 }
 
